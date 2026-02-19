269d848aca3e87ae002cce6568b7aabd67420e36
Júlia Wiggeshoff <130909048+juliawiggeshoff@users.noreply.github.com>
2026-02-19 10:16:40 +0100

    Output final table from preprocess.hrd.R
    Result can be used in future scar_score() runs in non-seqz mode

--- R/preprocess.hrd.R
+++ R/preprocess.hrd.R
@@ -3,6 +3,11 @@
 #' @param seg segmentation data
 #' @return preprocessed data
 preprocess.hrd<-function(seg){
+  # Output file that can be used in non-seqz run in the future  # 
+  outputdir = getwd()
+  run_name <- unique(seg$SampleID)
+  out_file <- paste0(outputdir,"/",run_name,"_preprocessed_hrd.txt")
+  
   seg <- seg[!seg[,2] %in% c(paste('chr',c('X','Y','x','y',23,24),sep=''),c('X','Y','x','y',23,24)),]
   seg[,1] <- as.character(seg[,1])
 
@@ -12,6 +17,7 @@ preprocess.hrd<-function(seg){
     seg[tmp[,8] > tmp[,7],8]  <- tmp[tmp[,8] > tmp[,7],7]
   }
   seg <- shrink.seg.ai.wrapper(seg)
+  write.table(seg,file = out_file, sep="\t",quote = FALSE, row.names = FALSE)
+  
   return(seg)
-
 }
